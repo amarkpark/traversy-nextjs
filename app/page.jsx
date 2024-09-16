02e24478454f40c2087fb8b4dc99618015ae9239
Amanda Mark <amarkpark@gmail.com>
2024-09-16 17:14:05 -0600

    Lesson 10: add properties routes

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,10 @@
+import Link from "next/link";
+
 const HomePage = () => {
   return ( 
     <div>
       <h1 className="text-3xl font-bold">Home Page</h1>
+      <Link href="/properties">Go To Properties</Link>
     </div>
    );
 }
---
+++ app/properties/[id]/page.jsx
@@ -0,0 +1,7 @@
+const PropertyDetailPage = () => {
+  return ( 
+    <div>PropertyDetailPage</div>
+   );
+}
+ 
+export default PropertyDetailPage;
