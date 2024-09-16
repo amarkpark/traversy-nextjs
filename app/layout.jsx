3be0efd12b917e2479c8a3203fa9a69409b378b5
Amanda Mark <amarkpark@gmail.com>
2024-09-16 16:55:27 -0600

    Lesson 9: Adding Assets
    For now I have added the "/public/images/properties/" path to .gitignore
    in order to keep the images local until we figure out where they will
    eventually live.

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,11 @@
 import "@/assets/styles/globals.css";
 
+export const metadata = {
+  title: "Traversy Property Pulse",
+  keywords: "rental, property, real estate",
+  description: "https://www.udemy.com/course/nextjs-from-scratch",
+}
+
 const RootLayout = ({ children }) => {
   return ( <html>
     <body>
