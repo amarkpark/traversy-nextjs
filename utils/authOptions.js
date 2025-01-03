be2305e9effdf84cf46f1bfa51dfae8270beeef9
Amanda Mark <amarkpark@gmail.com>
2025-01-03 15:40:18 -0700

    Lesson 32: Wire up Auth Callbacks Db and Session

--- utils/authOptions.js
+++ utils/authOptions.js
@@ -1,4 +1,6 @@
 import GoogleProvider from "next-auth/providers/google";
+import connectDB from "@/config/database";
+import User from "@/models/User";
 
 export const authOptions = {
   // Configure one or more authentication providers
@@ -20,17 +22,35 @@ export const authOptions = {
   ],
   callbacks: {
     // Invoked on successful sign in
+    // I feel like this should be called "verifyUserOrCaptureNewUserData"
     async signIn({ profile }) {
       // 1. Connect to the Db
+      await connectDB();
       // 2. Check if user exists
+      const userExists = await User.findOne({ email: profile.email });
       // 3. Else create user
+      if (!userExists) {
+        // Truncate username
+        const username = profile.name.slice(0, 20);
+
+        // @TODO this needs error-handling try-catch
+        await User.create({
+          email: profile.email,
+          username,
+          image: profile.picture,
+        });
+      }
       // Return "true" to allow sign in
+      return true;
     },
     // Session callback function that modifies session object
     async session({ session }) {
       // 1. Get user from Db
+      const currentUser = await User.findOne({email: session.user.email});
       // 2. Assign user id from the session
+      session.user.id = currentUser._id.toString();
       // 3. Return the session
+      return session;
     }
   }
 }
